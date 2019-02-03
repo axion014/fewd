9c412f44873c14ad62eacd42ee0ff4d577bd165b
axion014 <axion014@gmail.com>
2019-02-03 21:55:55 +0900

    implement hitTestEllipse

--- hittest.js
+++ hittest.js
@@ -3,8 +3,8 @@ export function hitTestCircle(dx, dy) {
 }
 
 export function hitTestEllipse(dx, dy) {
-	if (this.radius) return hitTestCircle.call(this, dx, dy);
-	// TODO
+	if (this.radius || this.width === this.height) return hitTestCircle.call(this, dx, dy);
+	return Math.hypot(dx / this.width, dy / this.height) < 1;
 }
 
 export function hitTestRectangle(dx, dy) {

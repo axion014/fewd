eea5836ace7dcdc003e5c82fe17948065e59156d
axion014 <axion014@gmail.com>
2019-03-21 09:36:59 +0900

    stroke on SymmetricTriangle

--- geometries.js
+++ geometries.js
@@ -179,26 +179,66 @@ shape.lineTo(0.5, -0.5);
 shape.lineTo(-0.5, -0.5);
 shape.lineTo(0, 0.5);
 const symmetrictrianglegeometry = new ShapeBufferGeometry(shape);
+const symmetrictrianglelinegeometry = (w, h) => [0, h, w, -h, -w, -h, 0, h];
 export class SymmetricTriangle extends Element {
 	constructor(options) {
 		options = options || {};
-
-		super(new Mesh(
+		const group = new Group();
+		group.fill = new Mesh(
 			symmetrictrianglegeometry,
 			new MeshBasicMaterial({color: options.fillColor})
-		), options);
+		);
+		group.fill.visible = !!options.fillColor;
+		group.add(group.fill);
 
-		this.nativeContent.visible = !!options.fillColor;
+		group.stroke = createMeshLine(
+			symmetrictrianglelinegeometry(options.width / 2, options.height / 2),
+			{
+				color: options.strokeColor,
+				lineWidth: options.strokeWidth !== undefined ? options.strokeWidth : 2
+			},
+			true
+		);
+		group.stroke.visible = !!options.strokeColor;
+		group.add(group.stroke);
+
+		super(group, options);
+
+		this.addEventListener("render", () => {
+			if (this._dirty) setMeshLineGeometry(group.stroke, symmetrictrianglelinegeometry(group.fill.scale.x / 2, group.fill.scale.y / 2), true);
+			this._dirty = false;
+		});
 	}
-	get width() {return this.nativeContent.scale.x}
-	set width(v) {this.nativeContent.scale.x = v}
+	get width() {return this.nativeContent.fill.scale.x}
+	set width(v) {
+		this.nativeContent.fill.scale.x = v;
+		this._dirty = true;
+	}
 
-	get height() {return this.nativeContent.scale.y}
-	set height(v) {this.nativeContent.scale.y = v}
+	get height() {return this.nativeContent.fill.scale.y}
+	set height(v) {
+		this.nativeContent.fill.scale.y = v;
+		this._dirty = true;
+	}
 
-	get fillColor() {return this.nativeContent.material.color}
+	get fillOpacity() {return this.nativeContent.fill.opacity}
+	set fillOpacity(v) {this.nativeContent.fill.opacity = v}
+
+	get strokeOpacity() {return this.nativeContent.stroke.opacity}
+	set strokeOpacity(v) {this.nativeContent.stroke.opacity = v}
+
+	get strokeWidth() {return this.nativeContent.stroke.material.uniforms.lineWidth.value}
+	set strokeWidth(v) {this.nativeContent.stroke.material.uniforms.lineWidth.value = v}
+
+	get fillColor() {return this.nativeContent.fill.material.color}
 	set fillColor(v) {
-		this.nativeContent.visible = Boolean(v);
-		this.nativeContent.material.color.set(v)
+		this.nativeContent.fill.visible = Boolean(v);
+		this.nativeContent.fill.material.color.set(v)
+	}
+
+	get strokeColor() {return this.nativeContent.stroke.material.uniforms.color.value}
+	set strokeColor(v) {
+		this.nativeContent.stroke.visible = Boolean(v);
+		this.nativeContent.stroke.material.uniforms.color.value.set(v)
 	}
 }

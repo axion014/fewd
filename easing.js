8f6cf71ed8fc1aa7852cbaeefeb776550a872362
axion014 <axion014@gmail.com>
2019-03-23 22:41:07 +0900

    various minor easing functions

--- easing.js
+++ easing.js
@@ -97,10 +97,26 @@ export default class Easing {
 	}
 
 	static in(d) {
+		if (d === SINE) return t => 1 - Math.cos(t * Math.PI / 2);
+		if (d === CIRC) return t => 1 - Math.sqrt(1 - t * t);
+		if (d === EXPO) return t => t === 0 ? t : Math.pow(2, 10 * (t - 10));
+		if (d === ELASTIC) return t => Math.sin(13 * t * Math.PI / 2) * Math.pow(2, 10 * (t - 10));
+		if (d === BOUNCE) return Easing.out(BOUNCE);
 		return t => Math.pow(t, d);
 	}
 
 	static out(d) {
+		if (d === SINE) return t => Math.sin(t * Math.PI / 2);
+		if (d === CIRC) return t => Math.sqrt(1 - (--t * t));
+		if (d === EXPO) return t => t === 1 ? t : 1 - Math.pow(2, -10 * t);
+		if (d === ELASTIC) return t => Math.sin(-13 * (t + 1) * Math.PI / 2) * Math.pow(2, -10 * t) + 1;
+		if (d === BOUNCE) return t => {
+			const t2 = t * t;
+			if (t < 0.363636363636364) return 7.5625 * t2;
+			if (t < 0.727272727272727) return 9.075 * t2 - 9.9 * t + 3.4;
+			if (t < 0.9) return 12.066481994459834 * t2 - 19.635457063711911 * t + 8.898060941828255;
+			return 10.8 * t * t - 20.52 * t + 10.72;
+		};
 		return t => 1 - Math.pow(1 - t, d);
 	}
 
@@ -109,4 +125,10 @@ export default class Easing {
 		const out = out(d);
 		return t => t < 0.5 ? in(t * 2) / 2 : 1 - out(2 - t * 2) / 2;
 	}
+
+	static SINE = {};
+	static CIRC = {};
+	static EXPO = {};
+	static ELASTIC = {};
+	static BOUNCE = {};
 }

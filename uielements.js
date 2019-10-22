447c938b96d235f8247472b67269b013ad919943
axion014 <axion014@gmail.com>
2019-10-22 19:00:29 +0900

    remove unused copy of getFontHeight in three-text2d

--- uielements.js
+++ uielements.js
@@ -9,23 +9,6 @@ import {define, defineAccessor} from "./utils";
 import Element from "./element";
 import {hitTestRectangle} from './hittest';
 
-const fontHeightCache = {};
-function getFontHeight(fontStyle) {
-  let result = fontHeightCache[fontStyle];
-  if (!result) {
-    const body = document.getElementsByTagName('body')[0];
-    const dummy = document.createElement('div');
-    const dummyText = document.createTextNode('MÉq');
-    dummy.appendChild(dummyText);
-    dummy.setAttribute('style', "font:" + fontStyle + ";position:absolute;top:0;left:0");
-    body.appendChild(dummy);
-    result = dummy.offsetHeight;
-    fontHeightCache[fontStyle] = result;
-    body.removeChild(dummy);
-  }
-  return result;
-}
-
 class ModifiedSpriteText2D extends SpriteText2D {
 	constructor(text, options) {
 		options = options || {};

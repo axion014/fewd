59cb4ed49e120df887c26a14094761345e88690e
axion014 <axion014@gmail.com>
2020-04-14 20:34:59 +0900

    fix

--- infiniteof.js
+++ infiniteof.js
@@ -6,24 +6,26 @@ import {vw, vh} from "./main";
 export default class Infiniteof extends Element {
 	constructor(generator, interval, options) {
 		super(new Group(), options);
+		this.interval = interval;
 		this.addEventListener('render', () => {
 	    const globalpos = this.localToWorld(this.position);
-	    const backrate = -Math.floor(Math.min((globalpos.x - (interval.x < 0 ? vw : 0)) / interval.x,
-				(globalpos.y - (interval.y < 0 ? vh : 0)) / interval.y));
+	    const backrate = -Math.floor(Math.min(
+				(globalpos.x - (this.interval.x < 0 ? vw : 0)) / this.interval.x,
+				(globalpos.y - (this.interval.y < 0 ? vh : 0)) / this.interval.y));
 
-	    const base = interval.clone().multiplyScalar(backrate + 1);
+	    const base = this.interval.clone().multiplyScalar(backrate + 1);
 			let i = -1;
-	    for(const pos = interval.clone().multiplyScalar(backrate - 1);
+	    for(const pos = this.interval.clone().multiplyScalar(backrate - 1);
 					pos.x - base.x < vw && pos.x - base.x > -vw && pos.y - base.y < vh && pos.y - base.y > -vh;
-	        pos.add(this.pitch), i++) {
+	        pos.add(this.interval), i++) {
 	      if (i + backrate >= this.nodemin && i + backrate < this.nodemax) continue;
 	      const node = this.add(generator(i + backrate));
-	      node.position = pos.clone();
+	      node.position.copy(pos);
 	      node._i = i + backrate;
 
 				// update before it gets rendered for the first time
 	      node.dispatchEvent({type: 'update'});
-				node.update();
+				if (node.update) node.update();
 	    }
 	    this.nodemin = backrate - 1;
 	    this.nodemax = i + backrate;

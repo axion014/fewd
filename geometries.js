a99c9bc3cff6761ba6a362ee54bb7e20dba96872
axion014 <axion014@gmail.com>
2020-08-08 15:44:04 +0900

    refactor geometries

--- geometries.js
+++ geometries.js
@@ -8,18 +8,19 @@ import {
 import {createMeshLine, setMeshLineGeometry} from "./threeutil";
 import Element from "./element";
 import {hitTestRectangle, hitTestEllipse} from './hittest';
+import {defineAccessor} from './utils';
 
 export class GeometricElement extends Element {
-	constructor(options, dimensiontovertices, fillgeometry) {
+	constructor(options, vertices, geometry) {
 		const group = new Group();
 
-		group.fill = new Mesh(fillgeometry, new MeshBasicMaterial({color: options.fillColor}));
+		group.fill = new Mesh(geometry(options), new MeshBasicMaterial({color: options.fillColor}));
 		group.fill.visible = !!options.fillColor;
 		group.fill.opacity = options.fillOpacity;
 		group.add(group.fill);
 
 		group.stroke = createMeshLine(
-			dimensiontovertices(options.width / 2, options.height / 2, options),
+			vertices(options),
 			{
 				color: options.strokeColor,
 				lineWidth: options.strokeWidth !== undefined ? options.strokeWidth : 2
@@ -33,19 +34,27 @@ export class GeometricElement extends Element {
 		super(group, options);
 
 		this.addEventListener("render", () => {
-			if (this._dirty) setMeshLineGeometry(group.stroke, dimensiontovertices(group.fill.scale.x / 2, group.fill.scale.y / 2, this), true);
-			this._dirty = false;
+			if (this._dirty) {
+				this.updateGeometry();
+				this._dirty = false;
+			}
 		});
 	}
-	get width() {return this.nativeContent.fill.scale.x}
+
+	updateGeometry() {
+		group.fill.geometry = geometry(this);
+		setMeshLineGeometry(group.stroke, vertices(this), true);
+	}
+
+	get width() {return this._width}
 	set width(v) {
-		this.nativeContent.fill.scale.x = v;
+		this._width = v;
 		this._dirty = true;
 	}
 
-	get height() {return this.nativeContent.fill.scale.y}
+	get height() {return this._height}
 	set height(v) {
-		this.nativeContent.fill.scale.y = v;
+		this._height = v;
 		this._dirty = true;
 	}
 
@@ -71,42 +80,61 @@ export class GeometricElement extends Element {
 	}
 }
 
+export function setScaleToResize(c) {
+	defineAccessor(c.prototype, "width", {
+		get: function() {return this.nativeContent.fill.scale.x},
+		set: function(v) {this.nativeContent.fill.scale.x = v}
+	});
+	defineAccessor(c.prototype, "height", {
+		get: function() {return this.nativeContent.fill.scale.y},
+		set: function(v) {this.nativeContent.fill.scale.y = v}
+	});
+	c.prototype.updateGeometry = function() {
+		setMeshLineGeometry(this.nativeContent.stroke, vertices(this), true);
+	};
+}
+
 const rectgeometry = new PlaneBufferGeometry(1, 1);
-const rectlinegeometry = (w, h) => [-w, h, w, h, w, -h, -w, -h, -w, h];
+function rectlinegeometry(self) {
+	const w = self.width / 2, h = self.height / 2;
+	return [-w, h, w, h, w, -h, -w, -h, -w, h];
+}
 export class Rectangle extends GeometricElement {
 	constructor(options) {
 		options = Object.assign({width: 10, height: 10}, options);
 
-		super(options, rectlinegeometry, rectgeometry);
+		super(options, rectlinegeometry, () => rectgeometry);
 
 		this.hitTest = hitTestRectangle;
 	}
 }
+setScaleToResize(Rectangle);
 
-const circlegeometries = {};
-function getCircleGeometry(segments) {
-	if (!circlegeometries[segments]) circlegeometries[segments] = new CircleBufferGeometry(0.5, segments);
-	return circlegeometries[segments];
-}
-const ellipselinegeometry = (w, h, self) => {
+function arcvertices(x, y, w, h, s, b, e) {
 	const g = [];
-	const s = self.segments;
 	for (let i = 0; i <= s; i++) {
-		const r = i / s * Math.PI * 2;
-		g.push(Math.cos(r) * w, Math.sin(r) * h);
+		const r = b + i / s * (e - b);
+		g.push(x + Math.cos(r) * w, y + Math.sin(r) * h);
 	}
 	return g;
-};
+}
+
+const circlegeometries = {};
+function circlegeometry(self) {
+	const s = self.segments;
+	if (!circlegeometries[s]) circlegeometries[s] = new CircleBufferGeometry(0.5, s);
+	return circlegeometries[s];
+}
+const ellipselinegeometry = self => arcvertices(0, 0, self.width / 2, self.height / 2, self.segments, 0, Math.PI * 2);
 export class Ellipse extends GeometricElement {
 	constructor(options) {
-		options = Object.assign({width: 10, height: 10}, options);
+		options = Object.assign({width: 10, height: 10, segments: 32}, options);
 
-		if (options.segments === undefined) options.segments = 32;
 		if (options.radius) options.width = options.height = options.radius * 2;
 
-		super(options, ellipselinegeometry, getCircleGeometry(options.segments));
+		super(options, ellipselinegeometry, circlegeometry);
 
-		this._segments = options.segments;
+		this.segments = options.segments;
 
 		this.hitTest = hitTestEllipse;
 	}
@@ -120,11 +148,11 @@ export class Ellipse extends GeometricElement {
 
 	get segments() {return this._segments}
 	set segments(v) {
-		this.nativeContent.fill.geometry = getCircleGeometry(v);
 		this._segments = v;
 		this._dirty = true;
 	}
 }
+setScaleToResize(Ellipse);
 
 export function verticesToGeometry(vertices) {
 	const shape = new Shape();
@@ -133,11 +161,15 @@ export function verticesToGeometry(vertices) {
 	return new ShapeBufferGeometry(shape);
 }
 
-const symmetrictrianglelinegeometry = (w, h) => [0, h, w, -h, -w, -h, 0, h];
-const symmetrictrianglegeometry = verticesToGeometry(symmetrictrianglelinegeometry(0.5, 0.5));
+function symmetrictrianglelinegeometry(self) {
+	const w = self.width / 2, h = self.height / 2;
+	return [0, h, w, -h, -w, -h, 0, h];
+}
+const symmetrictrianglegeometry = verticesToGeometry(symmetrictrianglelinegeometry({width: 1, height: 1}));
 export class SymmetricTriangle extends GeometricElement {
 	constructor(options) {
 		options = Object.assign({width: 10, height: 10}, options);
-		super(options, symmetrictrianglelinegeometry, symmetrictrianglegeometry);
+		super(options, symmetrictrianglelinegeometry, () => symmetrictrianglegeometry);
 	}
 }
+setScaleToResize(SymmetricTriangle);
